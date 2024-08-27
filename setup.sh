78ae0f6ba4cc2bd3830440c9a0922e7816686c6e
lawMOB <83550208+LawMOB@users.noreply.github.com>
2024-08-27 12:11:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt upgrade -y
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt install sudo wget nala jq flameshot conky-all libvulkan1 glmark2 -y
 
 #Install DRI3 patched driver
-wget https://github.com/bengkelgawai/Termux_XFCE/raw/main/mesa-vulkan-kgsl_23.3.0-ubuntu_arm64.deb
+wget https://github.com/LawMOB/Termux_XFCE_Qualcomm/raw/main/mesa-vulkan-kgsl_24.2.0-ubuntu_arm64.deb
 mv mesa-vulkan-kgsl_24.2.0-ubuntu_arm64.deb $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/root/
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 dpkg -i mesa-vulkan-kgsl_24.2.0-ubuntu_arm64.deb
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm mesa-vulkan-kgsl_24.2.0-ubuntu_arm64.deb
